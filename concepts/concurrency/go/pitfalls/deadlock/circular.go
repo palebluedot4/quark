ac8ba2235fe4e703b1e64f9ed83593b32a14a324
Bruce <162610581+palebluedot4@users.noreply.github.com>
2026-02-02 09:44:03 +0800

    feat(go): add functions to demonstrate deadlock scenarios
    Introduce two functions, BadInconsistentLockOrder and
    GoodConsistentLockOrder, to illustrate the impact of lock
    ordering on deadlocks in concurrent programming. The first
    function demonstrates a bad practice that can lead to deadlocks
    by acquiring locks in inconsistent order, while the second
    function shows a good practice with consistent lock ordering
    to avoid such issues. These examples serve as educational
    tools for understanding concurrency pitfalls in Go.

---
+++ concepts/concurrency/go/pitfalls/deadlock/circular.go
@@ -0,0 +1,51 @@
+package deadlock
+
+import "sync"
+
+func BadInconsistentLockOrder() {
+	var (
+		wg  sync.WaitGroup
+		mu1 sync.Mutex
+		mu2 sync.Mutex
+	)
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		mu1.Lock()
+		defer mu1.Unlock()
+		mu2.Lock()
+		defer mu2.Unlock()
+	}()
+	go func() {
+		defer wg.Done()
+		mu2.Lock()
+		defer mu2.Unlock()
+		mu1.Lock()
+		defer mu1.Unlock()
+	}()
+	wg.Wait()
+}
+
+func GoodConsistentLockOrder() {
+	var (
+		wg  sync.WaitGroup
+		mu1 sync.Mutex
+		mu2 sync.Mutex
+	)
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		mu1.Lock()
+		defer mu1.Unlock()
+		mu2.Lock()
+		defer mu2.Unlock()
+	}()
+	go func() {
+		defer wg.Done()
+		mu1.Lock()
+		defer mu1.Unlock()
+		mu2.Lock()
+		defer mu2.Unlock()
+	}()
+	wg.Wait()
+}
