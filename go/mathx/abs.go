9538afb449364f81c4d4be61ea8d53ec7b063ed4
Bruce <162610581+palebluedot4@users.noreply.github.com>
2026-05-23 10:24:09 +0800

    feat(go): add AbsFloat in mathx

--- go/mathx/abs.go
+++ go/mathx/abs.go
@@ -1,10 +1,18 @@
 package mathx
 
-import "golang.org/x/exp/constraints"
+import (
+	"math"
+
+	"golang.org/x/exp/constraints"
+)
 
 func AbsSigned[T constraints.Signed](x T) T {
 	if x < 0 {
 		return -x
 	}
 	return x
+}
+
+func AbsFloat[T constraints.Float](x T) T {
+	return T(math.Abs(float64(x)))
 }
