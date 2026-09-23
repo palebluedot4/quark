60745eccd05b3677aa788e1b0222724b09eb15f1
Bruce <162610581+palebluedot4@users.noreply.github.com>
2026-09-23 14:27:47 +0800

    feat(go): add tournament top-two selection
    Use the tournament method: pair values off in knockout rounds, then
    take the runner-up from the values the champion beat. That takes
    n + ceil(log2 n) - 2 comparisons, versus 2n - 3 in the worst case for
    a linear scan.

--- go/selection/toptwo.go
+++ go/selection/toptwo.go
@@ -20,3 +20,42 @@ func TopTwoLinear[S ~[]E, E cmp.Ordered](s S) (first, second E) {
 	}
 	return first, second
 }
+
+type contender[T any] struct {
+	value    T
+	defeated []T
+}
+
+func TopTwoTournament[S ~[]E, E cmp.Ordered](s S) (first, second E) {
+	if len(s) < 2 {
+		panic("selection.TopTwoTournament: slice too short")
+	}
+	contenders := make([]contender[E], len(s))
+	for i, v := range s {
+		contenders[i] = contender[E]{value: v}
+	}
+	for len(contenders) > 1 {
+		next := make([]contender[E], 0, (len(contenders)+1)/2)
+		for i := 0; i+1 < len(contenders); i += 2 {
+			winner, loser := i, i+1
+			if cmp.Less(contenders[winner].value, contenders[loser].value) {
+				winner, loser = loser, winner
+			}
+			advancing := contenders[winner]
+			advancing.defeated = append(advancing.defeated, contenders[loser].value)
+			next = append(next, advancing)
+		}
+		if len(contenders)%2 == 1 {
+			next = append(next, contenders[len(contenders)-1])
+		}
+		contenders = next
+	}
+	champion := contenders[0]
+	first, second = champion.value, champion.defeated[0]
+	for _, candidate := range champion.defeated[1:] {
+		if cmp.Less(second, candidate) {
+			second = candidate
+		}
+	}
+	return first, second
+}
